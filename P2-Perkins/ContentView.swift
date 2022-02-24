fda2da3ba07dbf911a134bc65155f6846b281335
Charles Antonelli <cja@umich.edu>
2022-02-24 17:50:48 -0500

    Initial Commit

---
+++ P2-Perkins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P2-Perkins
+//
+//  Created by Charles Antonelli on 2/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P2-Perkins/P2_PerkinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  P2_PerkinsApp.swift
+//  P2-Perkins
+//
+//  Created by Charles Antonelli on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct P2_PerkinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
